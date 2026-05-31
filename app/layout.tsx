ff1f23dbdfa7f3305b9fce688d99fd3147746dd8
Lieuwe Jongsma <lieuwe89@gmail.com>
2026-05-31 22:14:40 +0200

    chore: add OpenTracker snippet (v0.14.3)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import { ReactNode } from "react";
+import Script from "next/script";
+
+const openTrackerUrl = "https://www.lieuwejongsma.nl/wp-content/plugins/open-tracker/assets/js/ot-tracker.js";
+const openTrackerRestUrl = "https://www.lieuwejongsma.nl/wp-json/open-tracker/v1";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>{children}</body>
+      <Script
+        src={openTrackerUrl}
+        data-ot-rest-url={openTrackerRestUrl}
+        strategy="afterInteractive"
+      />
     </html>
   );
 }
